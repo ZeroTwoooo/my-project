d34cd8410e751846e3f0b7438e97a2e823a72ae1
ZeroTwoooo <45253268+ZeroTwoooo@users.noreply.github.com>
2020-06-14 02:58:45 -0500

    lol ok

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,20 @@
+var username
 const text_area = document.getElementById("main-text-area")
+const send = () => {
+    if ( username == undefined ) {
+        username = "User1"
+    }
+    const user_text = document.getElementById("user_input_text")
+    const text = document.createElement("p")
+    text.id = "unique-text-cfaf"
+    text.className = "unique-user-text"
+    text_area.appendChild(text)
+    text.innerHTML = username +": "+ user_text.value
+    setTimeout(() => {
+        text.parentNode.removeChild(text)
+    },5000)
+}
+const set_username = () => {
+    username = document.getElementById("user_input_username").value
+}
+
