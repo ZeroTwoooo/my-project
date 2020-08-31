5df0027933e3daa54c3c96afa490bfa796efc4a0
ZeroTwoooo <45253268+ZeroTwoooo@users.noreply.github.com>
2020-08-31 17:06:39 -0500

    Your are

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,34 @@
+const makeid = (length) => {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 var username
+const user_id = makeid(100)
+console.log(user_id)
 
 const text_area = document.getElementById("main-text-area")
 
 const send = () => {
-    if (username == undefined) {
-        username = "User1"
+    if (username == undefined || username == "") {
+        username = "User-"+makeid(5)
     }
+
     const user_text = document.getElementById("user_input_text")
-    const text = document.createElement("p")
-    text.id = "unique-text-cfaf"
-    text.className = "unique-user-text"
-    text_area.appendChild(text)
-    text.innerHTML = username + ": " + user_text.value
-    setTimeout(() => {
-        text.parentNode.removeChild(text)
-    }, 10000)
+        const text = document.createElement("p")
+
+        text.id = "unique-text-cfaf"
+        text.className = "unique-user-text"
+        text_area.appendChild(text)
+        text.innerHTML = username + ": " + user_text.value
+
+        setTimeout(() => {
+            text.parentNode.removeChild(text)
+        }, 10000)
 }
 
 const set_username = () => {
